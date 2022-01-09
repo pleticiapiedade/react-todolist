b5e29894a2f87f18336fcf171c77c4023d822c4e
Leticia Piedade <pleticiapiedade@gmail.com>
2022-01-09 17:18:37 -0300

    :tada: To do List - full project

---
+++ src/App.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #17181F;
+    color: #797A81;
+    min-height: 100vh;
+`;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px;
+`;
+
+export const Header = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #FFF;
+    border-bottom: solid 1px #444;
+    text-align: center;
+    padding-bottom: 20px;
+    padding-top: 20px;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import {ListItem} from './components/ListItem';
+import { AddArea } from './components/AddArea';
+
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Lavar os sapatos', done: false},
+    {id: 2, name: 'Comprar açucar', done: true}
+  ])
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+        id: list.length + 1,
+        name: taskName,
+        done: false
+    });
+    setList(newList);
+}
+//change task status 
+const handleTaskChange = (id: number, done: boolean) => {
+  let newList = [...list];
+  for (let i in newList){
+    if (newList[i].id === id){
+      newList[i]. done = done;
+    }
+  }
+  setList(newList);
+}
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        
+        <AddArea onEnter={handleAddTask}/>
+
+        {
+          list.map((item, index)=> (
+            <ListItem key={index} item={item} onChange={handleTaskChange}/>
+          ))
+        }
+
+
+      </C.Area>
+    </C.Container>
+  );
+}
+
+export default App;
